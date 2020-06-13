f4288ee910b6e798500281a94ada907d4533cfad
Thiago M. Nóbrega <42742390+mmtmn@users.noreply.github.com>
2020-06-13 09:03:44 -0300

    del error prone rust web assembly

--- projects/basics/target/debug/deps/basics.d
+++ projects/basics/target/debug/deps/basics.d
@@ -1,6 +1,6 @@
-C:\Users\thiag\documents\github\rust-side-project-1\projects\basics\target\debug\deps\basics.exe: src\main.rs src\hello.rs src\vars.rs src\types.rs
+C:\Users\mmtmn\documents\github\rust-side-project-1\projects\basics\target\debug\deps\basics.exe: src\main.rs src\hello.rs src\vars.rs src\types.rs
 
-C:\Users\thiag\documents\github\rust-side-project-1\projects\basics\target\debug\deps\basics.d: src\main.rs src\hello.rs src\vars.rs src\types.rs
+C:\Users\mmtmn\documents\github\rust-side-project-1\projects\basics\target\debug\deps\basics.d: src\main.rs src\hello.rs src\vars.rs src\types.rs
 
 src\main.rs:
 src\hello.rs:
